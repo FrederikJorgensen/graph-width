97bcb977828b34c932fd274e89b3f24bce0b48bd
Frederik Jørgensen <fjvemmedrup@gmail.com>
2020-04-06 18:27:22 +0200

    added random graph and changed layout

--- public/js/td.js
+++ public/js/td.js
@@ -1,95 +1,224 @@
 let graphNode;
 let graphLink;
+let graphLabel;
 const adjlist = [];
+let selected = [];
 let graph1;
+let finalGraph;
+const graphInput = document.getElementById('graph-input');
+
+/** Sample a random graph G(n,m)
+  * @param {Number} n vertices
+  * @param {Number} m edges
+  * @return {Graph}
+  */
+function randomGraph(n, m) {
+  const maxNumEdges = n * (n - 1) / 2;
+  if (n < 0 || m < 0 || m > maxNumEdges) return undefined;
+
+  const graph = { nodes: [], links: [] };
+  for (let i = 0; i < n; i++) {
+    graph.nodes[i] = { id: i };
+  }
+
+  const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+  const state = {};
+  for (let i = 0; i < m; i++) {
+    const j = randomInt(i, maxNumEdges);
+    if (!(i in state)) state[i] = i;
+    if (!(j in state)) state[j] = j;
+    [state[i], state[j]] = [state[j], state[i]];
+  }
+
+  function unpair(k) {
+    const z = Math.floor((-1 + Math.sqrt(1 + 8 * k)) / 2);
+    return [k - z * (1 + z) / 2, z * (3 + z) / 2 - k];
+  }
+
+  for (let i = 0; i < m; i++) {
+    const [x, y] = unpair(state[i]);
+    const u = graph.nodes[x];
+    const v = graph.nodes[n - 1 - y];
+    graph.links.push({ source: u, target: v });
+  }
+  return graph;
+}
+
 
 /* eslint-disable no-param-reassign */
-function drawGraph(data) {
+function drawGraph(graph) {
   const width = document.getElementById('graph-container').offsetWidth;
   const height = document.getElementById('graph-container').offsetHeight;
   const svg = d3.select('#graph');
 
-  d3.json(data).then((graph) => {
-    const { nodes } = graph;
-    const { links } = graph;
+  const { nodes } = graph;
+  const { links } = graph;
 
-    graphLink = svg.append('g')
-      .selectAll('line')
-      .data(links)
-      .enter()
-      .append('line')
-      .attr('class', 'link');
+  graphLink = svg.append('g')
+    .selectAll('line')
+    .data(links)
+    .enter()
+    .append('line')
+    .attr('class', 'link');
 
-    graphNode = svg.append('g')
-      .selectAll('g')
-      .data(nodes)
-      .enter()
-      .append('circle')
-      .attr('r', 18)
-      .attr('class', 'node');
+  graphNode = svg.append('g')
+    .selectAll('g')
+    .data(nodes)
+    .enter()
+    .append('circle')
+    .attr('r', 18)
+    .attr('class', 'node');
 
-    const graphLabel = svg
-      .append('g')
-      .selectAll('text')
-      .data(nodes)
-      .enter()
-      .append('text')
-      .attr('class', 'label')
-      .attr('text-anchor', 'middle')
-      .text((d) => d.label);
+  graphLabel = svg
+    .append('g')
+    .selectAll('text')
+    .data(nodes)
+    .enter()
+    .append('text')
+    .attr('class', 'label')
+    .attr('text-anchor', 'middle')
+    .text((d) => d.label);
 
-    function ticked() {
-      graphNode.attr('cx', (d) => d.x)
-        .attr('cy', (d) => d.y);
+  function ticked() {
+    graphNode.attr('cx', (d) => d.x)
+      .attr('cy', (d) => d.y);
 
-      graphLink.attr('x1', (d) => d.source.x)
-        .attr('y1', (d) => d.source.y)
-        .attr('x2', (d) => d.target.x)
-        .attr('y2', (d) => d.target.y);
+    graphLink.attr('x1', (d) => d.source.x)
+      .attr('y1', (d) => d.source.y)
+      .attr('x2', (d) => d.target.x)
+      .attr('y2', (d) => d.target.y);
 
-      graphLabel
-        .attr('x', (d) => d.x)
-        .attr('y', (d) => d.y);
+    graphLabel
+      .attr('x', (d) => d.x)
+      .attr('y', (d) => d.y);
+  }
+
+  const simulation = d3.forceSimulation(nodes)
+    .force('charge', d3.forceManyBody().strength(-500))
+    .force('center', d3.forceCenter(width / 2, height / 2))
+    .force('link', d3.forceLink(links).id((d) => d.id).distance(50).strength(1))
+    .on('tick', ticked);
+
+
+  function dragstarted(d) {
+    d3.event.sourceEvent.stopPropagation();
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+
+  function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+
+  function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
+
+  links.forEach((d) => {
+    adjlist[`${d.source.index}-${d.target.index}`] = true;
+  });
+
+  graphNode.call(
+    d3.drag()
+      .on('start', dragstarted)
+      .on('drag', dragged)
+      .on('end', dragended),
+  );
+
+  graph1 = graph;
+}
+
+function readSingleFile(evt) {
+  const f = evt.target.files[0];
+  const edges = [];
+
+  const r = new FileReader();
+  r.onload = function onLoad() {
+    const lines = this.result.split('\n');
+    const list = [];
+    for (let line = 0; line < lines.length; line++) {
+      const textLine = lines[line];
+      if (textLine.startsWith('c') || textLine.startsWith('p') || textLine.length < 1) {
+        continue;
+      } else {
+        const firstNode = parseInt(textLine[0]);
+        const secondNode = parseInt(textLine[2]);
+        list.push(firstNode);
+        list.push(secondNode);
+        edges.push({ source: firstNode, target: secondNode });
+      }
     }
 
-    const simulation = d3.forceSimulation(nodes)
-      .force('charge', d3.forceManyBody().strength(-500))
-      .force('center', d3.forceCenter(width / 2, height / 2))
-      .force('link', d3.forceLink(links).id((d) => d.id).distance(50).strength(1))
-      .on('tick', ticked);
-
-
-    function dragstarted(d) {
-      d3.event.sourceEvent.stopPropagation();
-      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
+    const sortedList = [...new Set(list)];
+    const finalList = [];
+    for (let i = 0; i < sortedList.length; i++) {
+      finalList.push({ id: sortedList[i] });
     }
 
-    function dragged(d) {
-      d.fx = d3.event.x;
-      d.fy = d3.event.y;
-    }
+    const verticesAsString = JSON.stringify(finalList);
+    const edgesAsString = JSON.stringify(edges);
+    const nodes = `${'"nodes"' + ': '}${verticesAsString}`;
+    const links = `${'"links"' + ': '}${edgesAsString}`;
+    finalGraph = `{${nodes},${links}}`;
+    const jsonGraph = JSON.parse(finalGraph);
+    drawGraph(jsonGraph);
+  };
+  r.readAsText(f);
+}
 
-    function dragended(d) {
-      if (!d3.event.active) simulation.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
-    }
+graphInput.addEventListener('change', readSingleFile, false);
 
-    links.forEach((d) => {
-      adjlist[`${d.source.index}-${d.target.index}`] = true;
+function nodeShouldBeVisible(nodeIndex, vertices) {
+  return vertices.some((vertix) => adjlist[`${vertix}-${nodeIndex}`] || adjlist[`${nodeIndex}-${vertix}`] || vertix === nodeIndex);
+}
+
+function linkShouldBeVisible(sourceIndex, targetIndex, indexes) {
+  if (targetIndex !== undefined) {
+    return nodeShouldBeVisible(sourceIndex, indexes)
+          && nodeShouldBeVisible(targetIndex, indexes);
+  }
+  return indexes.some((d) => adjlist[`${sourceIndex}-${d}`]
+           + adjlist[`${d}-${sourceIndex}`]);
+}
+
+function getVerticesIndexes(vertices) {
+  graph1.nodes.forEach((currentNode) => {
+    if (vertices.includes(currentNode.label)) {
+      selected.push(currentNode.index);
+    }
+  });
+  return selected;
+}
+
+function highlightNodes(hoveredNode) {
+  const verticesToHighlight = hoveredNode.data.vertices;
+  const indexes = getVerticesIndexes(verticesToHighlight);
+
+  graphNode
+    .attr('opacity', (o) => {
+      if (nodeShouldBeVisible(o.index, indexes)) {
+        return 1;
+      }
+      return 0.1;
     });
 
-    graphNode.call(
-      d3.drag()
-        .on('start', dragstarted)
-        .on('drag', dragged)
-        .on('end', dragended),
-    );
-
-    graph1 = graph;
+  graphLink.attr('opacity', (o) => {
+    if (linkShouldBeVisible(o.source.index, o.target.index, indexes)) {
+      return 1;
+    }
+    return 0;
   });
+}
+
+function resetHighlight() {
+  graphNode.attr('opacity', 1);
+  graphLink.attr('opacity', 1);
+  selected = [];
 }
 
 
@@ -129,7 +258,7 @@ function drawTree(data) {
     const root = d3.hierarchy(graph);
     const links = root.links();
     const nodes = root.descendants();
-    let selected = [];
+
 
     const simulation = d3.forceSimulation(nodes)
       .force('link', d3.forceLink(links).id((d) => d.id).distance(2).strength(0.5))
@@ -165,55 +294,6 @@ function drawTree(data) {
       .attr('class', 'label')
       .text((d) => d.data.name);
 
-    function nodeShouldBeVisible(nodeIndex, vertices) {
-      return vertices.some((vertix) => adjlist[`${vertix}-${nodeIndex}`] || adjlist[`${nodeIndex}-${vertix}`] || vertix === nodeIndex);
-    }
-
-    function linkShouldBeVisible(sourceIndex, targetIndex, indexes) {
-      if (targetIndex !== undefined) {
-        return nodeShouldBeVisible(sourceIndex, indexes)
-              && nodeShouldBeVisible(targetIndex, indexes
-                );
-      }
-      return indexes.some((d) => adjlist[`${sourceIndex}-${d}`]
-               + adjlist[`${d}-${sourceIndex}`]);
-    }
-
-    function getVerticesIndexes(vertices) {
-      graph1.nodes.forEach((currentNode) => {
-        if (vertices.includes(currentNode.label)) {
-          selected.push(currentNode.index);
-        }
-      });
-      return selected;
-    }
-
-    function highlightNodes(hoveredNode) {
-      const verticesToHighlight = hoveredNode.data.vertices;
-      const indexes = getVerticesIndexes(verticesToHighlight);
-
-      graphNode
-        .attr('opacity', (o) => {
-          if (nodeShouldBeVisible(o.index, indexes)) {
-            return 1;
-          }
-          return 0.1;
-        });
-
-      graphLink.attr('opacity', (o) => {
-        if (linkShouldBeVisible(o.source.index, o.target.index, indexes)) {
-          return 1;
-        }
-        return 0;
-      });
-    }
-
-    function resetHighlight() {
-      graphNode.attr('opacity', 1);
-      graphLink.attr('opacity', 1);
-      selected = [];
-    }
-
     node.on('mouseover', highlightNodes);
     node.on('mouseout', resetHighlight);
 
@@ -241,5 +321,97 @@ function drawTree(data) {
   });
 }
 
-drawGraph('../../data/graph.json');
+function create() {
+  if (graphNode && graphLink && graphLabel) {
+    graphNode.remove();
+    graphLink.remove();
+    graphLabel.remove();
+  }
+  const graph = randomGraph(20, 20);
+  drawGraph(graph);
+}
+
+document.getElementById('reload').addEventListener('click', create);
+
+
+const graphin = {
+  nodes: [
+    {
+      id: 0,
+      label: 'a',
+    },
+    {
+      id: 1,
+      label: 'b',
+    },
+    {
+      id: 2,
+      label: 'c',
+    },
+    {
+      id: 3,
+      label: 'd',
+    },
+    {
+      id: 4,
+      label: 'e',
+    },
+    {
+      id: 5,
+      label: 'f',
+    },
+    {
+      id: 6,
+      label: 'g',
+    },
+    {
+      id: 7,
+      label: 'h',
+    },
+  ],
+  links: [
+    {
+      source: 0,
+      target: 1,
+    },
+    {
+      source: 0,
+      target: 3,
+    },
+    {
+      source: 1,
+      target: 6,
+    },
+    {
+      source: 6,
+      target: 7,
+    },
+    {
+      source: 6,
+      target: 4,
+    },
+    {
+      source: 7,
+      target: 5,
+    },
+    {
+      source: 5,
+      target: 2,
+    },
+    {
+      source: 2,
+      target: 3,
+    },
+    {
+      source: 5,
+      target: 4,
+    },
+    {
+      source: 6,
+      target: 3,
+    },
+  ],
+};
+
+drawGraph(graphin);
 drawTree('../../data/tree.json');
