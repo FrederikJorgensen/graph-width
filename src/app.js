dc1c2eb93c958638111a98f4149d48e81670d521
Frederik Jørgensen <fjvemmedrup@gmail.com>
2020-07-05 11:28:47 +0200

    parcel update

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(fileupload());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../dist')));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
